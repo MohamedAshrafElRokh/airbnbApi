9314c7b27c4c2e81dae3fab7907eee70e403aa7a
MohamedAshrafElRokh <mohamed.ashraf.elrokh@gmail.com>
2023-09-12 13:39:25 +0300

    first commit

---
+++ index.js
@@ -0,0 +1,36 @@
+const express = require("express");
+const cors = require('cors');
+const User = require("./models/Users.js")
+const bcrypt = require('bcryptjs')
+const bcryptSalt = bcrypt.genSaltSync(10)
+const app = express();
+const mongoose = require("mongoose");
+require('dotenv').config()
+
+app.use(express.json())
+
+app.use(cors({
+
+    credentials:true,
+    origin:'http://localhost:5173'
+}))
+
+mongoose.connect(process.env.MONGO_URL);
+
+app.get('/test',(req,res)=>
+{
+    res.json("hi")
+})
+
+app.post('/register', async(req,res)=>
+{
+    const {name, email, password} = req.body;
+    const userData = await User.create({
+        name,
+        email,
+        password:bcrypt.hashSync(password, bcryptSalt),
+    })
+    res.json(userData);
+})
+
+app.listen(8000)
