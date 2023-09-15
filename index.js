6e5e4469e1620e14a2e19c41b1620a827804bdc5
MohamedAshrafElRokh <mohamed.ashraf.elrokh@gmail.com>
2023-09-15 05:03:15 +0300

    forgot to add error,token in sign function

--- index.js
+++ index.js
@@ -12,18 +12,20 @@ const bcryptSalt = bcrypt.genSaltSync(10)
 const jwtSecret = "sdaadasdadawdadad"
 app.use(express.json())
 
-// app.use(cors({
+app.use(
+cors({
+    credentials: true,
+    origin: "http://localhost:5173",
+})
+);
 
-//     credentials:true,
-//     origin:'http://localhost:5173'
-// }))
-
-app.use(cors({ origin: "*" }));
+// app.use(cors({ origin: "*" }));
 
 mongoose.connect(process.env.MONGO_URL);
 
 app.post('/register', async(req,res)=>
 {
+    console.log("hi");
     try
     {
         const {name, email, password} = req.body;
@@ -31,12 +33,12 @@ app.post('/register', async(req,res)=>
             name,
             email,
             password:bcrypt.hashSync(password, bcryptSalt),
-    })
+        })
+        res.json(userData);
     } catch(error)
     {
         res.status(422).json(error)
     }
-    res.json(userData);
 })
 
 app.post('/login',async(req,res)=>
@@ -46,25 +48,25 @@ app.post('/login',async(req,res)=>
         const {email,password} = req.body;
         console.log(res.body);
         const userData = await User.findOne({email})
-
         if(userData)
         {
             const passOk = bcrypt.compareSync(password,userData.password)
+            console.log(userData, "api");
             if(passOk)
             {
-                jwt.sign({email:userData.email, id:userData._id}, jwtSecret, {}, () =>
+                jwt.sign({email:userData.email, id:userData._id}, jwtSecret, {}, (error,token) =>
                 {
                     if(error)   throw error
                     res.cookie('token', token).json(true)
+                    
                 })
-
             }else{
                 res.json(false)
             }
         }
         else
         {
-            res.json('not found')
+            res.json(false)
         }
     }
     catch(error)
